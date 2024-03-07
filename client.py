9a839e5a546ba07ee660b1bbb89c055365332225
itzelseneca <158524415+itzelseneca@users.noreply.github.com>
2024-03-07 04:02:50 +0000

    testing

--- client.py
+++ client.py
@@ -1,30 +1,13 @@
-import socket  
-import pickle  
+"""
+Write a program to count the number of vowels in a string.
+"""
 
-# function to serialize file data
-def serializeFile(filePath):
-    with open(filePath, 'r') as file:  # open the file in read mode
-        fileData = {'filename': filePath, 'data': file.read()}  # read the file data and store it along with filename
-    return pickle.dumps(fileData)  # serialize the file data using pickle
+vowels = ['a','i','u','e','o']
+mystring = "big boy bonglous"
+counter = 0
 
-# function to run the client
-def runClient():
-    try:
-        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP socket
-        serverAddress = ('127.0.0.1', 12345)  # server address and port
-        clientSocket.connect(serverAddress)  # connecting to the server
+for x in mystring:
+    if x in vowels:
+        counter+=1
 
-        filePath = input("Enter the file path of the file to be transferred: ")  # asking user to input file path
-        fileToSend = serializeFile(filePath)  # serializing the file to send
-        clientSocket.sendall(fileToSend)  # sending the serialized file to the server
-
-        print("File sent successfully.")  
-        
-    except Exception as err: 
-        print(f"Error: {err}")  # Printing the error message if any
-
-    finally:
-        clientSocket.close()  # closing the client socket after execution
-
-if __name__ == "__main__":
-    runClient()  # calling the function to run the client
+print("ur strring got this many vowels: ", counter)
